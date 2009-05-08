088ced0d85f969e98c4d348e73c7e8c4ff9d0171
scognito <scognito@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-05-08 22:20:51 +0000

    Interface for per game settings

---
+++ CoverFlow/source/gameSettings.c
@@ -0,0 +1,16 @@
+#include "gameSettings.h"
+
+s_gameSettings gameSetting;
+
+void getGameSettings(char* gameCode, s_gameSettings* gs){
+
+	
+	//else
+	gs = NULL;
+}
+
+void setGameSettings(char* gameCode, s_gameSettings* gs){
+
+}
+
+
