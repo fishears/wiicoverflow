daa501b8a610f4fc0ca3f6d0c5560c5248c0c435
justin.hawkins <justin.hawkins@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-05-12 19:07:18 +0000

    Mute sounds on button held for graphics settings. (Issue 88)

--- CoverFlow/include/button.h
+++ CoverFlow/include/button.h
@@ -17,6 +17,7 @@ typedef struct Button {
 Button Button_Init(const unsigned char normal_img[], const unsigned char hover_img[], int x, int y);
 bool Button_Hover(struct Button* btn, int x, int y);
 bool Button_Select(struct Button* btn, int x, int y);
+bool Button_Select_Mute(struct Button* btn, int x, int y);
 void Button_Paint(struct Button* btn);
 void Button_Theme_Paint(struct Button* btn, int theme_id);
 void Button_Toggle_Paint(struct Button* btn1, struct Button* btn2, int toggle_state);
