91fac09c6a76b20f91b48eedfcc1750583ccf960
gc <gc@blackbird99.demon.co.uk>
2009-05-13 21:30:16 +0000

    New buffer code complete

--- CoverFlow/source/cover.c
+++ CoverFlow/source/cover.c
@@ -238,9 +238,9 @@ void batchDownloadCover(struct discHdr *gameList)
 			//sprintf(filepath, USBLOADER_PATH "/covers/%s.png", header->id);
 		}
 	}
-	BUFFER_ClearCovers();
+//	BUFFER_ClearCovers();
 	Sleep(300);
-	UpdateBufferedImages();
+//	UpdateBufferedImages();
 	Sleep(100);
 	WindowPrompt ("Operation finished!","Press A to continue", &okButton, 0);
 }
