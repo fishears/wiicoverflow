659a98e11a4acd2088202ccc9d4f1334d78aa779
alexcarlosantao <alexcarlosantao@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-05-15 13:05:55 +0000

    Added calls for Localization function.

--- trunk/CoverFlow/source/utils.c
+++ trunk/CoverFlow/source/utils.c
@@ -24,65 +24,65 @@ void sysdate(char* ret_string){
 	switch(ts->tm_wday){
 	
 		case 0:
-			sprintf(dow, "Sun");
+			sprintf(dow, localStr("M112", "Sun") );
 			break;
 		case 1:
-			sprintf(dow, "Mon");
+			sprintf(dow, localStr("M113", "Mon") );
 			break;
 		case 2:
-			sprintf(dow, "Tue");
+			sprintf(dow, localStr("M114", "Tue") );
 			break;
 		case 3:
-			sprintf(dow, "Wed");
+			sprintf(dow, localStr("M115", "Wed") );
 			break;
 		case 4:
-			sprintf(dow, "Thu");
+			sprintf(dow, localStr("M116", "Thu") );
 			break;
 		case 5:
-			sprintf(dow, "Fri");
+			sprintf(dow, localStr("M117", "Fri") );
 			break;
 		case 6:
-			sprintf(dow, "Sat");
+			sprintf(dow, localStr("M118", "Sat") );
 			break;
 	}
 	
 	switch(ts->tm_mon){
 		
 		case 0:
-			sprintf(month, "Jan");
+			sprintf(month, localStr("M119", "Jan") );
 			break;
 		case 1:
-			sprintf(month, "Feb");
+			sprintf(month, localStr("M120", "Feb") );
 			break;
 		case 2:
-			sprintf(month, "Mar");
+			sprintf(month, localStr("M121", "Mar") );
 			break;
 		case 3:
-			sprintf(month, "Apr");
+			sprintf(month, localStr("M122", "Apr") );
 			break;
 		case 4:
-			sprintf(month, "May");
+			sprintf(month, localStr("M123", "May") );
 			break;
 		case 5:
-			sprintf(month, "Jun");
+			sprintf(month, localStr("M124", "Jun") );
 			break;
 		case 6:
-			sprintf(month, "Jul");
+			sprintf(month, localStr("M125", "Jul") );
 			break;
 		case 7:
-			sprintf(month, "Aug");
+			sprintf(month, localStr("M126", "Aug") );
 			break;
 		case 8:
-			sprintf(month, "Sep");
+			sprintf(month, localStr("M127", "Sep") );
 			break;
 		case 9:
-			sprintf(month, "Oct");
+			sprintf(month, localStr("M128", "Oct") );
 			break;
 		case 10:
-			sprintf(month, "Nov");
+			sprintf(month, localStr("M129", "Nov") );
 			break;
 		case 11:
-			sprintf(month, "Dec");
+			sprintf(month, localStr("M130", "Dec") );
 			break;
 		//default:
 		//	sprintf(month, "xxx: %d", ts->tm_mon);
@@ -101,8 +101,8 @@ void ios_version_check()
     if(iosrev <REQUIRED_IOS_REV)
     {
         char string[80];
-        sprintf(string, "Current: cIOS Rev%d  Required: cIOS Rev%d",iosrev, REQUIRED_IOS_REV);
-        WindowPrompt("cIOS Upgrade Required",string, &okButton, 0);
+        sprintf(string, localStr("M131", "Current: cIOS Rev%d  Required: cIOS Rev%d"),iosrev, REQUIRED_IOS_REV);
+        WindowPrompt(localStr("M132", "cIOS Upgrade Required"),string, &okButton, 0);
     }
 }
 
@@ -150,7 +150,7 @@ int Net_Init(char *ip){
 		usleep(100 * 1000); //100ms
         
 	if (if_config(ip, NULL, NULL, true) < 0) {
-		WindowPrompt ("ERROR!","Cannot get local IP address.", &okButton, 0);
+		WindowPrompt (localStr("M003", "ERROR!"), localStr("M085", "Cannot get local IP address."), &okButton, 0);
 		usleep(1000 * 1000 * 1); //1 sec
 		
 		return false;
