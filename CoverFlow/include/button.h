de89c4589c216cfea384aaaf2e18df1c602f4b77
LoudBob11 <LoudBob11@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-05-19 16:12:10 +0000

    Localization: next steps for labeling buttons Current: disabled

--- CoverFlow/include/button.h
+++ CoverFlow/include/button.h
@@ -2,10 +2,19 @@
 #define _Button_H_
 
 //comment out for Localization-Buttons
-#define LOCALBUTTON 
+#define LOCALBUTTON_OFF
 
 #include "GRRLIB.h"
 
+
+#ifndef LOCALBUTTON_OFF
+//Button-Type
+	#define BTN_NORMAL      1
+	#define BTN_NORMAL_HOV  2
+	#define BTN_BIG         3
+	#define BTN_BIG_HOV     4
+	#define BTN_TOOLTIP		5
+#endif
 
 typedef struct Button {
 	int x;
@@ -15,7 +24,7 @@ typedef struct Button {
 	struct GRRLIB_texImg hoverTexture;
 		
 	bool hovering;
-#ifndef 	LOCALBUTTON
+#ifndef 	LOCALBUTTON_OFF
 	char label[15];  //add for Localization
 #endif	
 } Button;
@@ -27,6 +36,10 @@ bool Button_Select_Mute(struct Button* btn, int x, int y);
 void Button_Paint(struct Button* btn);
 void Button_Theme_Paint(struct Button* btn, int theme_id);
 void Button_Toggle_Paint(struct Button* btn1, struct Button* btn2, int toggle_state);
+
+#ifndef 	LOCALBUTTON_OFF
+void Button_Overlay_Text(f32 xpos, f32 ypos, int btnType, char *text);
+#endif
 
 #endif //_BUTTON_H_
 
