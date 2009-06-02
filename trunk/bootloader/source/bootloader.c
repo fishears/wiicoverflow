4c20d4cfd5e802046b2fcabc77136c0147520d95
F1SHE4RS <F1SHE4RS@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-06-02 13:52:26 +0000

    Committing the revised bootloader that applies patches to the game

--- trunk/bootloader/source/bootloader.c
+++ trunk/bootloader/source/bootloader.c
@@ -1,7 +1,23 @@
 #include "bootloader.h"
 
+extern s_settings settings;
+s_gameSettings gameSetting;
 
 u32 swap32(u32 x)
 {
 	return (x >> 24) | ((x << 8) & 0x00FF0000UL) | ((x >> 8) & 0x0000FF00UL) | (x << 24);
+}
+
+void apply_settings()
+{
+    //called after global settings are saved and right before game is launched
+    //this overwrites global settings with the per-game settings
+    if(gameSetting.ocarina != -1)
+    {
+        settings.ocarina = gameSetting.ocarina;
+        settings.hooktype = gameSetting.hooktype;
+        settings.language = gameSetting.language;
+        settings.video = gameSetting.video;
+        settings.vipatch = gameSetting.vipatch;
+    }
 }
