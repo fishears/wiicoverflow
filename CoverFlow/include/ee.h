4db1efdf1a503b0f2f92ebc3ac63844e87b09cdd
afour98 <afour98@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-06-09 04:37:32 +0000

    for when we get ee() working... still dumps when trying to load a game

--- CoverFlow/include/ee.h
+++ CoverFlow/include/ee.h
@@ -5,7 +5,7 @@
 #include "utils.h"
 //#include "soundmanager.h"
 
-#define AUTHORS_LEN 6
+#define AUTHORS_LEN 7
 
 extern const u8 ee_sco_png[];
 extern const u8 ee_justin_png[];
@@ -13,6 +13,7 @@ extern const u8 ee_fishears_png[];
 extern const u8 ee_alex_png[];
 extern const u8 ee_bob_png[];
 extern const u8 ee_bbird_png[];
+extern const u8 ee_afour_png[];
 
 typedef struct{
 	int sx;
