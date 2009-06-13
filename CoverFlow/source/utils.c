39cf9c5dd7df73c24e7fcba5ce691e54a2a17cbe
afour98 <afour98@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-06-13 08:22:05 +0000

    fixin the other complain fish had in r593... now the 3D covers zoom out/in when they flip, so you can zoom all the way in and read the back (if it's a good scan).
    The flip transition could be smoother (I tried to do a little easing) but I'll leave that to someone else... I'm tired and going to bed.

--- CoverFlow/source/utils.c
+++ CoverFlow/source/utils.c
@@ -362,4 +362,9 @@ float easeOutCubic(float t, float b , float c, float d)
 	return c*((t=t/d-1)*t*t + 1) + b;
 }
 
+float easeInOutCubic(float t, float b , float c, float d)
+{
+	if ((t/=d/2) < 1) return c/2*t*t*t + b;
+	return c/2*((t-=2)*t*t + 2) + b;
+}
 
