5bf32a21b920a012f045c9bc3ab559c08808557c
blackbird399 <blackbird399@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-06-16 23:12:46 +0000

    Forgot to byte align the memory to 32 bytes

--- CoverFlow/source/buffer.c
+++ CoverFlow/source/buffer.c
@@ -55,8 +55,9 @@ int GraphicModes[GRAPHIC_MODES][2] =
 };
 
 // this is the chunk of MEM2 allocated for other tasks (i.e. things that don't fit in normal memory)
-//#define BUFFER_SLOTS 10
-#define BUFFER_SLOTS 8
+#define BUFFER_SLOTS 10
+
+
 unsigned int FreeMemorySlots[BUFFER_SLOTS+1] =
 	{
 	128,//0 string buffer for thread 0
@@ -67,8 +68,8 @@ unsigned int FreeMemorySlots[BUFFER_SLOTS+1] =
 	512*340*4, // 5 3d no cover
 	240*480*4, //6 ambilight
 	160*224*4, //7 no cover
-//	500, //8 Cheat Manager Array 1
-//	1800, //9 Cheat Manager Array 2
+	512, //8 Cheat Manager Array 1
+	1920, //9 Cheat Manager Array 2
 	0};
 
 // returns the offset to the memory slot required
