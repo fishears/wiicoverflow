9e8b05bede3dab32f369228911431dbf2617d823
F1SHE4RS <F1SHE4RS@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-06-29 11:10:09 +0000

    Fix Issue 269 - Adds credit for the use of GRRLib

--- trunk/CoverFlow/source/settingsMenu.c
+++ trunk/CoverFlow/source/settingsMenu.c
@@ -844,6 +844,7 @@ void Settings_Menu_Show()
 				CFreeTypeGX_DrawText(ttf14pt, 320, 180, "Licensed under the terms of the", (GXColor){0x22, 0x22, 0x22, 0xff}, FTGX_JUSTIFY_CENTER);
 				CFreeTypeGX_DrawText(ttf14pt, 320, 195, "GNU GPL version 2", (GXColor){0x22, 0x22, 0x22, 0xff}, FTGX_JUSTIFY_CENTER);
 				CFreeTypeGX_DrawText(ttf14pt, 320, 210, "http://code.google.com/p/wiicoverflow/", (GXColor){0x22, 022, 0x22, 0xff}, FTGX_JUSTIFY_CENTER);
+                                CFreeTypeGX_DrawText(ttf14pt, 320, 230, "CoverFloader uses the power of GRRLib", (GXColor){0x22, 022, 0x22, 0xff}, FTGX_JUSTIFY_CENTER);
 				CFreeTypeGX_DrawText(ttf18pt, 320, 260, "Additional Development By:", (GXColor){0x00, 0x00, 0x00, 0xff}, FTGX_JUSTIFY_CENTER);
 				CFreeTypeGX_DrawText(ttf16pt, 320, 280, "gitkua, scognito, F1SHE4RS, afour98,", (GXColor){0x22, 0x22, 0x22, 0xff}, FTGX_JUSTIFY_CENTER);
 				CFreeTypeGX_DrawText(ttf16pt, 320, 300, "blackbird399, LoudBob11, alexcarlosantao", (GXColor){0x22, 0x22, 0x22, 0xff}, FTGX_JUSTIFY_CENTER);
