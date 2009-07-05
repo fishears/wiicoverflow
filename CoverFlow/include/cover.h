2fcc8ae62760f808e0cd66385500c630acdc3b84
scognito <scognito@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-07-05 13:24:43 +0000

    [Weekend Project] Improved regional cover downloading (thanks to suggestion of frederic.martini) fix issue311. Now covers are downloaded following these steps: 1) try to get cover based on game ID 2) fall back to system language if not found on previous step 3) fall back to english if not found on previous step

--- CoverFlow/include/cover.h
+++ CoverFlow/include/cover.h
@@ -7,6 +7,7 @@ void LoadCurrentCover(int id, struct discHdr *gameList);
 void AddCover(GRRLIB_texImg tex);
 void Init_Covers();
 void Download_Cover(char* id, int v, int max);
+bool getCoverFromServer(char* url, char* imgPath, int v, int max);
 void batchDownloadCover(struct discHdr *gameList);
 
 #endif
