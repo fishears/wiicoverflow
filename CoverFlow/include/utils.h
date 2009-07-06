722169aa37af5c540540074127213e84dc70e8f5
blackbird399 <blackbird399@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-07-06 16:57:12 +0000

    New Function for parsing token urls

--- CoverFlow/include/utils.h
+++ CoverFlow/include/utils.h
@@ -33,5 +33,6 @@ float easeInQuint(float t,float b , float c, float d);
 float easeOutQuad(float t,float b , float c, float d);
 float easeInQuad(float t,float b , float c, float d);
 float easeOutCubic(float t,float b , float c, float d);
+char * ParseTokenedUrl(char * url, char* username, char* password, char * lang, char * region, char *id);
 
 #endif
