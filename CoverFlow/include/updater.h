320c008a27938b52e3058b04f7275d8f00a145c3
scognito <scognito@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-07-11 16:54:56 +0000

    doing an update without the updater files isn't easy, thanks Loudbob

---
+++ CoverFlow/include/updater.h
@@ -0,0 +1,25 @@
+/*
+ *  updater.h
+ *
+ *  Wii CoverFloader
+ *  Copyright 2009 Beardface April 29th, 2009
+ *  Additional coding by: gitkua, scognito, F1SHE4RS, afour98, blackbird399, LoudBob11, alexcarlosantao
+ *  Licensed under the terms of the GNU GPL, version 2
+ *  http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ *
+ *  This file contains the main() entry point to the application
+ *  Build configuration settings can be found in "coverflow.h"
+ */
+
+#ifndef _UPDATER_H
+#define	_UPDATER_H
+
+#include "coverflow.h"
+
+void* networkinitcallback(void *arg);
+void initNetworkThread();
+void shutdownNetworkThread();
+bool checkForUpdate();
+bool promptForUpdate();
+
+#endif
