b7fd78590fd23cb9cefdca708bad20f95f97aa4b
blackbird399 <blackbird399@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-07-27 20:56:23 +0000

    test for loudbob

--- trunk/CoverFlow/source/buffer.c
+++ trunk/CoverFlow/source/buffer.c
@@ -58,7 +58,7 @@ int GraphicModes[GRAPHIC_MODES][2] =
 };
 
 // this is the chunk of MEM2 allocated for other tasks (i.e. things that don't fit in normal memory)
-#define BUFFER_SLOTS 15
+#define BUFFER_SLOTS 16
 
 
 unsigned int FreeMemorySlots[BUFFER_SLOTS+1] =
@@ -78,6 +78,7 @@ unsigned int FreeMemorySlots[BUFFER_SLOTS+1] =
 	128,//12 string buffer tokenurlParsing
 	160*160*4, //13 no_disc_texture
 	472*172*4, //14 menu_graphics_wireframe_texture
+	2*1024*1024, //15 alt dol buffer
 	0};
 
 // returns the offset to the memory slot required
