2df69b24f2f234372ae4cdd058336e366c9e84fe
LoudBob11 <LoudBob11@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-08-02 13:06:14 +0000

    Internal test with F1SHE4RS and blackbird399 ------------------------------------------------------------ - added   int DebTxt( char * Msg) for adding Debug-Messages   to file. (utils.c) - enable #define DEBUG_FILE in defines.h -(with additional timestamp it seems that we're out of memory)

--- trunk/CoverFlow/include/utils.h
+++ trunk/CoverFlow/include/utils.h
@@ -34,5 +34,5 @@ float easeOutQuad(float t,float b , float c, float d);
 float easeInQuad(float t,float b , float c, float d);
 float easeOutCubic(float t,float b , float c, float d);
 char * ParseTokenedUrl(char * url, char* username, char* password, char * lang, char * region, char *id);
-
+int DebTxt( char * Msg);
 #endif
