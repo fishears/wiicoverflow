10071bdfbe97fcdbaa75a5b804c593ce6bfcae89
LoudBob11 <LoudBob11@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-09-09 11:45:01 +0000

    editable GameID via OSK ----------------------- (Thanks to Scognito for teamwork)

--- trunk/CoverFlow/include/storage.h
+++ trunk/CoverFlow/include/storage.h
@@ -11,6 +11,7 @@ void checkFiles();
 void initWBFS();
 s32 __Menu_EntryCmp(const void *a, const void *b);
 s32 GetEntries();
+s32 GetEntriesSilent();
 bool Init_Game_List();
 void initUSBFS();
 bool check_write_access();
