4b16ba45ca21d566e8fee7ee13080a899f25505d
LoudBob11 <LoudBob11@d19a4d92-33b9-11de-9cad-a3574e5575ef>
2009-11-09 12:07:30 +0000

    - optimized BannerSound

--- trunk/CoverFlow/source/core/wbfs.c
+++ trunk/CoverFlow/source/core/wbfs.c
@@ -551,8 +551,8 @@ int WBFS_BannerSound(u8 *discid, SoundInfo *snd)
 	if (!d) return -1;
 	size = wbfs_extract_file(d, "opening.bnr", &banner);
 	
-	if (!banner) 
-		size = wbfs_extract_file(d, "OPENING.BNR", &banner);
+//	if (!banner) 
+//		size = wbfs_extract_file(d, "OPENING.BNR", &banner);
 	wbfs_close_disc(d);
 	if (!banner) return -1;
 
